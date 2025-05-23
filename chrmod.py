43a8979ca5c92615dbb6d5ca82ecdc23f11ac473
STBD1574 <101642151+STBD1574@users.noreply.github.com>
2025-05-23 23:25:06 +0800

    add debug tip

--- chrmod.py
+++ chrmod.py
@@ -3,11 +3,9 @@
 from .api import gui
 from .command import command_registry
 from .module import module_manager
-from .event import event_listener
 
 def main():
     command_registry.init()
     module_manager.init()
-    event_listener.init()
-
+    
     gui.set_left_corner_notify_msg("[DEBUG] mod loaded!")
